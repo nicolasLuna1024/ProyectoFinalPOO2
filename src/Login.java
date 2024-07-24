7783e22ca9ade958eb4c3796a7ea542e908570af
nicolas2 <nicolas.luna@epn.edu.ec>
2024-07-24 16:42:31 -0500

    Panel Login Creado Diseño

--- src/Login.java
+++ src/Login.java
@@ -1,2 +1,10 @@
-public class Login {
+import javax.swing.*;
+
+public class Login extends JFrame{
+    private JComboBox BoxTipoUsuario;
+    private JPasswordField passwordField1;
+    private JLabel IconoLogin;
+    private JLabel TipoUsuario;
+    private JTextField textUsuario;
+    private JButton ingresarButton;
 }
