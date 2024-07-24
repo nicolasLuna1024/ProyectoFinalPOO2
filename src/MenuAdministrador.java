fff851c1a16d4e1650cba2598127dd6e31f9348a
nicolas2 <nicolas.luna@epn.edu.ec>
2024-07-24 18:49:03 -0500

    Panel Menu de Administrador Creado con Diseño

---
+++ src/MenuAdministrador.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class MenuAdministrador {
+    private JButton gestionarClientesButton;
+    private JButton gestionarCanchasButton;
+    private JButton gestionarHorariosButton;
+}
