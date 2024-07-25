578cb8bc325507cf39443828b34b33c201a19dfa
nicolas2 <nicolas.luna@epn.edu.ec>
2024-07-24 19:42:49 -0500

    Panel de Agregar Cliente agregado ya con Diseño

---
+++ src/PanelAgregarCliente1.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class PanelAgregarCliente {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+    private JTextField textField4;
+    private JButton AGREGARButton;
+    private JButton verClienteButton;
+}
