94348a66a50c6c72848eb1a76401f8087ed912e2
nicolas2 <nicolas.luna@epn.edu.ec>
2024-07-24 19:54:58 -0500

    Panel de Agregar cliente

---
+++ src/PanelAgregarCliente.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class PanelAgregarCliente {
+    private JTextField textNombreCompleto;
+    private JTextField textEmail;
+    private JTextField textTelefono;
+    private JTextField textDireccion;
+    private JTextField textFechaDeNacimiento;
+    private JTextField textCedula;
+    private JButton agregarButton;
+    private JButton verClienteButton;
+}
