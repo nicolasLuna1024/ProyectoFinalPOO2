e125eab69512983a2927b640c4a9c99c4c9b1818
nicolas2 <nicolas.luna@epn.edu.ec>
2024-07-24 21:02:06 -0500

    Panel de Eliminar CLiente

--- src/PanelActualizarCliente.java
+++ src/PanelActualizarCliente.java
@@ -1,2 +1,12 @@
+import javax.swing.*;
+
 public class PanelActualizarCliente {
+    private JTextField textCedulaBuscar;
+    private JTextField textNombreActualizar;
+    private JTextField textEmailActualizar;
+    private JTextField textTelefonoActualizar;
+    private JTextField textDireccionActualizar;
+    private JTextField textFechaActualizar;
+    private JButton actualizarButton;
+    private JButton menuButton;
 }
---
+++ src/PanelEliminarCLiente.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class PanelEliminarCLiente {
+    private JTextField textCedulaEliminar;
+    private JButton eliminarButton;
+    private JButton menuButton;
+}
